6af8cf8e85c85a2950d13cd1abe203e78ce49eb5
Satvik Jain <satvikjain@users.noreply.huggingface.co>
2025-11-19 11:14:35 +0000

    fix: gradio interface

--- app.py
+++ app.py
@@ -100,12 +100,12 @@ with gr.Blocks(fill_height = True) as app:
         gr.Interface(
             fn = score.df_show,
             inputs = None,
-            outputs=gr.Dataframe(type="pandas", label="Scoreboard", headers = ["","",""]),
+            outputs = gr.Dataframe(type="pandas", label="Scoreboard"),
             live = True,
             allow_flagging = "never",
             clear_btn = None
-
         )
+
         
 
 app.launch(share = True)
